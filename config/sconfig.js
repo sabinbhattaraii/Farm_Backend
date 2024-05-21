d2b7befd7c8c90cab1f41c6d7a59b5c52bdf5fbf
Sabinbhattarai <sabin.bhattarai2012@gmail.com>
2024-05-21 11:06:32 +0545

    feat:Added some config

--- config/sconfig.js
+++ config/sconfig.js
@@ -5,3 +5,13 @@ config()
 export const port = process.env.PORT
 export const base_url = process.env.SERVER_BASE_URL || "localhost:8080";
 export const apiVersion = process.env.API_VERSION
+
+export const secretKey = process.env.SECRET_KEY;
+export const expiryIn = process.env.EXPIRY_IN;
+export const reset_expiry_in = process.env.RESET_EXPIRE_IN;
+
+export const tokenTypes = {
+    ACCESS: "access",
+    RESET_PASSWORD: "resetPassword",
+    VERIFY_EMAIL: "verifEmail"
+};
