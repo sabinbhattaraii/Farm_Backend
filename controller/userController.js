07a33b2461358225019fa1f2685727199d00a41c
Sabinbhattarai <sabin.bhattarai2012@gmail.com>
2024-06-03 13:44:43 +0545

    feat:Added the try catch block to registeration code

--- controller/userController.js
+++ controller/userController.js
@@ -11,53 +11,61 @@ import { throwError } from "../utils/throwError.js";
 import { generateToken } from "../utils/token.js";
 
 //Register
-export const createAuthUser = catchAsyncErrors(async (req,res) => {
-    const body = { ...req.body };
-	body.isVerified = false;
+export const createAuthUser = catchAsyncErrors(async (req, res) => {
+    try {
+        const body = { ...req.body };
+        body.isVerified = false;
 
-	const email = body.email;
-	const user = await userService.getSpecificUserByAny({ email });
-	const getAllUser = await Users.countDocuments();
-	body.userId = (getAllUser || 0) + 1;
+        const email = body.email;
+        const user = await userService.getSpecificUserByAny({ email });
+        const getAllUser = await Users.countDocuments();
+        body.userId = (getAllUser || 0) + 1;
 
-    // Check and Set default role if not provided
-    if (!body.roles || body.roles.some((role) => role.trim() === '')) {
-        body.roles = ["customer"];
+        // Check and Set default role if not provided
+        if (!body.roles || body.roles.some((role) => role.trim() === '')) {
+            body.roles = ["customer"];
+        }
+
+        if (user) {
+            throw throwError({
+                message: "Email already exists.",
+                statusCode: HttpStatus.UNAUTHORIZED,
+            });
+        } else {
+            const data = await userService.createUserService({ body });
+            delete data._doc.password;
+
+            const infoObj = { userId: data._id };
+            const token = await generateToken(infoObj, secretKey, expiryIn);
+
+            const tokenData = {
+                token,
+                userId: data._id,
+                type: tokenTypes.VERIFY_EMAIL,
+                expiration: getTokenExpiryTime(token).toLocaleString(),
+            };
+
+            await tokenService.createTokenService({ data: tokenData });
+
+            await sendEmailToVerify({
+                email,
+                token,
+                firstName: body.firstName,
+                lastName: body.lastName
+            });
+
+            successResponseData({
+                res,
+                message: "Verification mail has been sent.",
+                statusCode: HttpStatus.CREATED,
+                data,
+            });
+        }
+    } catch (error) {
+        console.error("Error Registering User:", error);
+        throw throwError({
+            statusCode: HttpStatus.BAD_REQUEST,
+            message: "Server Error",
+        });
     }
-
-	if (user) {
-    	throw throwError({
-        	message: "Email already exists.",
-        	statusCode: HttpStatus.UNAUTHORIZED,
-    	});
-	} else {
-    	const data = await userService.createUserService({ body });
-    	delete data._doc.password;
-
-    	const infoObj = { userId: data._id };
-    	const token = await generateToken(infoObj, secretKey, expiryIn);
-
-    	const tokenData = {
-        	token,
-        	userId: data._id,
-        	type: tokenTypes.VERIFY_EMAIL,
-        	expiration: getTokenExpiryTime(token).toLocaleString(),
-    	};
-   	 
-    	await tokenService.createTokenService({ data: tokenData });
-   	 
-    	await sendEmailToVerify({
-        	email,
-        	token,
-        	firstName: body.firstName,
-        	lastName: body.lastName
-    	});
-
-    	successResponseData({
-        	res,
-        	message: "Verification mail has been sent.",
-        	statusCode: HttpStatus.CREATED,
-        	data,
-    	});
-	}
 });
