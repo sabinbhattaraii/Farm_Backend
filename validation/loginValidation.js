7f7fe135f1cfc082a4315211a46bf7b04c464cc2
Sabinbhattarai <sabin.bhattarai2012@gmail.com>
2024-06-16 18:40:47 +0545

    feat:Added the validation code for logout schema

---
+++ validation/loginValidation.js
@@ -0,0 +1,22 @@
+import Joi from "joi";
+
+const loginValidationSchema = Joi.object()
+    .keys({
+        email: Joi.string().email().required().messages({
+            'string.empty': 'Email is required',
+            'string.email': 'Email must be a valid email address'
+        }),
+
+        password: Joi.string()
+            .min(8)
+            .max(20)
+            .required()
+            .pattern(new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,20}$"))
+            .messages({
+                'string.empty': 'Password is required',
+                'string.min': 'Password should have a minimum length of 8 characters'
+            }),
+    })
+    .unknown(false);
+
+export default loginValidationSchema; unknown
