372da5591d3024c9c24b1775a636a1fd8603a5d4
Sabinbhattarai <sabin.bhattarai2012@gmail.com>
2024-06-20 18:01:41 +0545

    feat:Updated the index to export the contact controller

--- controller/index.js
+++ controller/index.js
@@ -1,3 +1,4 @@
 export * as userController from "./userController.js";
 export * as aboutController from "./aboutController.js";
 export * as inquiryController from "./inquiryController.js";
+export * as contactController from "./contactController.js";
