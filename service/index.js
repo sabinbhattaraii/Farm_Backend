582ff17fb2aa241b8761a6c881e4c4f9eeefb3f7
Sabinbhattarai <sabin.bhattarai2012@gmail.com>
2024-06-20 19:43:53 +0545

    feat:Added the review service to the index

--- service/index.js
+++ service/index.js
@@ -3,3 +3,4 @@ export * as tokenService from './tokenService.js';
 export * as aboutService from './aboutService.js';
 export * as inquiryService from './inquiryService.js';
 export * as contactService from './contactService.js';
+export * as reviewService from './reviewService.js';
