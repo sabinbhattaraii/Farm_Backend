cff56afd9c10d6fd428d3a22b8e1f30162b2c04c
Sabinbhattarai <sabin.bhattarai2012@gmail.com>
2024-06-20 18:10:28 +0545

    feat:Added the validation logic to verify the contact

---
+++ validation/contactUsValidation.js
@@ -0,0 +1,26 @@
+const Joi = require('joi');
+
+const contactValidation = Joi.object().keys({
+    email: Joi.string()
+        .email()
+        .required()
+        .messages({
+            'string.email': 'Email must be a valid email address',
+            'string.required': 'Email is required',
+        }),
+    address: Joi.string()
+        .required()
+        .messages({
+            'string.required': 'Address is required',
+        }),
+    contact_number: Joi.string()
+        .pattern(/^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/)
+        .required()
+        .messages({
+            'string.pattern.base': 'Contact number must be in the format of (123) 456-7890',
+            'string.required': 'Contact number is required',
+        }),
+    description: Joi.string().allow('').optional(),
+});
+
+module.exports = contactValidation;
