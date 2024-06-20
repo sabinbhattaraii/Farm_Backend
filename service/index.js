43d75b6e90b5819c53f95cb1df49b3ea7f2eae6d
Sabinbhattarai <sabin.bhattarai2012@gmail.com>
2024-06-20 17:51:18 +0545

    feat:Added the service to the index

--- service/index.js
+++ service/index.js
@@ -2,3 +2,4 @@ export * as userService from './userService.js';
 export * as tokenService from './tokenService.js';
 export * as aboutService from './aboutService.js';
 export * as inquiryService from './inquiryService.js';
+export * as contactService from './contactService.js';
