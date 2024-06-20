f7858250b673135b3575f9d1a2d82952e769683b
Sabinbhattarai <sabin.bhattarai2012@gmail.com>
2024-06-20 13:15:42 +0545

    feat:Added the inquiry service to the index and exported

--- service/index.js
+++ service/index.js
@@ -1,3 +1,4 @@
 export * as userService from './userService.js';
 export * as tokenService from './tokenService.js';
 export * as aboutService from './aboutService.js';
+export * as inquiryService from './inquiryService.js';
