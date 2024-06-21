94569198b7a83384b100d818d755fc4a409c958c
Sabinbhattarai <sabin.bhattarai2012@gmail.com>
2024-06-21 12:23:37 +0545

    feat:Added the controller to index

--- controller/index.js
+++ controller/index.js
@@ -2,3 +2,4 @@ export * as userController from "./userController.js";
 export * as aboutController from "./aboutController.js";
 export * as inquiryController from "./inquiryController.js";
 export * as contactController from "./contactController.js";
+export * as reviewController from "./reviewController.js";
