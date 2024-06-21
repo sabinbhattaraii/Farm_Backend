b4b24521bfba803a57066160c4a4df1794dc174c
Sabinbhattarai <sabin.bhattarai2012@gmail.com>
2024-06-21 12:58:12 +0545

    feat:Wrote the sequelize migration file for the category feature

---
+++ migrations/20240621070901-create-category.cjs
@@ -0,0 +1,45 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Category', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      title: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        validate: {
+          notEmpty: true,
+        },
+        unique: true,
+      },
+      image: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        validate: {
+          notEmpty: true,
+          isUrl: true,
+        },
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('now'),
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('now'),
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Category');
+  },
+};
