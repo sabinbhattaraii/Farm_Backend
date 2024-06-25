b4f48cb6d4c395d64fa81eccb3eb8ac850f672bc
Sabinbhattarai <sabin.bhattarai2012@gmail.com>
2024-06-25 19:46:40 +0545

    feat:Added the select attribute to getall of booking

--- service/bookingService.js
+++ service/bookingService.js
@@ -42,9 +42,12 @@ export function getAllBookingService({
     select = "",
 }) {
     const queryOptions = {
-        attributes: select,
         where: find,
     };
+
+    if (select) {
+        queryOptions.select = select;
+    }
 
     if (sort) {
         queryOptions.order = [[sort]];
